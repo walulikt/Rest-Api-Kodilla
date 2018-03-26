52a7d14e92500dd022d4cb58d21e0db1d4abb5d4
walulikt <walulik.t@wp.pl>
2018-03-26 22:34:12 +0200

    Trello facade test-spring fix

--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
@@ -4,14 +4,20 @@ import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TrelloFacadeTestSuite {
 
     @Autowired
-    private TrelloMapper trelloMapper= new TrelloMapper();
+    private TrelloMapper trelloMapper;
 
     @Test
     public void trelloMaptoBoardTest()  {
